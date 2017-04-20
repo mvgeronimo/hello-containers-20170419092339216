3047cfa26f82fbcccb4fa64e49f68ca58c281d37
sudesh@sg.ibm.com <sudesh@sg.ibm.com>
2017-04-20 21:31:20 +0800

    changed to 169.46.26.199

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://169.46.28.236/';
+$config['base_url'] = 'http://169.46.26.199/';
 
 /*
 |--------------------------------------------------------------------------
